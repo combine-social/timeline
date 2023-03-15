8733ae8da230d0aaf14bb6607e7aa8ca7d319829
Anders Borch <anders@cyborch.com>
2023-03-15 11:50:46 +0100

    Styling for landing page.

--- apps/web/src/lib/components/login/login.svelte
+++ apps/web/src/lib/components/login/login.svelte
@@ -6,6 +6,9 @@
 <template>
 	<form on:submit|preventDefault={() => login(instanceURL)}>
 		<label for="instance">Mastodon instance:</label>
-		<input type="text" name="instance" bind:value={instanceURL} />
+		<input type="text" name="instance" placeholder="my.social" bind:value={instanceURL} />
+		<input type="submit" value="Start" />
 	</form>
 </template>
+
+<style src="./login.css"></style>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -3,6 +3,21 @@
 </script>
 
 <template>
-	<h1>Toot Tail</h1>
-	<Login />
+	<h1>Post Context</h1>
+	<h2>Fix reply count in your home timeline.</h2>
+	<div class="content">
+		<p>
+			Mastodon has a known issue with federation, which results in some replies to remote messages
+			not showing up in your home timeline.
+		</p>
+		<p>There is a solution to this. It is a really easy one:</p>
+		<ol>
+			<li>Enter your mastodon instance name.</li>
+			<li>Authorise the Post Context application.</li>
+			<li>There is no step 3!</li>
+		</ol>
+		<Login />
+	</div>
 </template>
+
+<style src="./page.css"></style>
