10c2eb3217515c3568c8fa7a77e222a7b51698de
Anders Borch <anders@cyborch.com>
2023-03-15 15:12:37 +0100

    Added success styling

--- apps/web/src/routes/success/+page.svelte
+++ apps/web/src/routes/success/+page.svelte
@@ -1,3 +1,9 @@
 <template>
-	<p>Login success!</p>
+	<h1>You are all set</h1>
+	<p>
+		You can go about your day now, knowing that you won't miss out on any replies from other people
+		any longer.
+	</p>
 </template>
+
+<style src="./success.css"></style>
