b207a2f3b28bb8bef82179d3543b942cda7ae70c
Anders Borch <anders@cyborch.com>
2023-03-29 13:53:50 +0200

    Added description of functionality. Added license and privacy policy. New and lighter layout.

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -3,20 +3,122 @@
 </script>
 
 <template>
-	<h1>Combine Social</h1>
-	<h2>Combine remote and local timelines</h2>
+	<div class="heading">combine.<br />social</div>
 	<div class="content">
-		<p>
-			Mastodon has a known issue with federation, which results in some replies to remote messages
-			not showing up in your home timeline.
-		</p>
-		<p>There is a solution to this, and it's a really easy one:</p>
-		<ol>
-			<li>Enter your mastodon instance name.</li>
-			<li>Authorise this application.</li>
-			<li>There is no step 3!</li>
-		</ol>
-		<Login />
+		<div>
+			<h3>Combine Remote And Local Timelines</h3>
+			<h1>
+				<span class="accent">It&rsquo;s a problem.</span> Not all relevant messages are relayed across
+				Mastodon servers.
+			</h1>
+			<p class="centered">
+				<span class="accent">Combine.social</span> offers an easy and simple solution to missing replies
+				in your home timeline or apparently empty profiles showing up in likes, boosts or follow requests
+				showing up in your notifications.
+			</p>
+
+			<button class="cta" on:click={() => (document.location.href = '#signup')}>Get Started</button>
+		</div>
+
+		<div>
+			<h2>The Root Cause</h2>
+			<p>
+				<strong>In a sentence:</strong> The root cause is that servers only send messages to followers.
+			</p>
+			<p>
+				One side-effect of that is that if a person who you do not follow replies to a (non-local)
+				message, then you do not see the reply. Similarly, any previously unknown user will show up
+				as never having posted any messages, when they first appear in your notifications.
+			</p>
+			<img src="/reply-replication-flow.png" alt="" />
+		</div>
+
+		<div>
+			<h2>The Solution</h2>
+			<p>
+				How does it work? The service that simply polls your home timeline and notifications and
+				fetches replies to all posts as well as all recent messages by any user who shows up in your
+				notifications (e.g. likes, boosts or follows).
+			</p>
+			<p>The flow goes something like this:</p>
+			<ol>
+				<li>Fetch all messages from for the last 24 hours.</li>
+				<li>Fetch all replies from the remote server.</li>
+				<li>Ask your server to pull each remote message.</li>
+				<li>For each reply, go to step 2.</li>
+			</ol>
+			<img src="/fix-reply-count-flow.png" alt="" />
+			<p>Similarly, for notifications, the flow goes something like this:</p>
+			<ol>
+				<li>Fetch all notifications from for the last 24 hours.</li>
+				<li>Do a WebFinger lookup of each user that created a notification.</li>
+				<li>Fetch the latest messages sent by each user.</li>
+				<li>Ask your server to pull each remote message.</li>
+				<li>For each message, perform steps 2-4 as above.</li>
+			</ol>
+		</div>
+
+		<div>
+			<h2>The Devil In The Detail</h2>
+			<p>
+				There is of course a devil in the detail, Mastodon has a rate limit on most API calls set to
+				300 requests every 5 minutes per user. The rate limit is upheld by making sure that the
+				fetch function is throttled to one request every two seconds (so we don't exclude the human
+				user from actually accessing those remote hosts by taking up the entire rate quota).
+			</p>
+			<p>
+				What this means for you is that it might take a while ( ~30 minutes or so) before you see
+				replies starting to show up after you first authorise this application.
+			</p>
+		</div>
+
+		<div>
+			<h2>Your Data Is Still Yours</h2>
+
+			<p>
+				<strong>The privacy policy:</strong> None of your messages are stored outside of your server.
+				A list of message URLs are queued up. URLs of replies to remote messages are fetched and queued
+				up as well.
+			</p>
+
+			<p>
+				The only thing we store is your username and an access token with read-only permissions.
+			</p>
+
+			<p>Nothing will ever be written to your account or posted on your behalf.</p>
+
+			<p>
+				If you de-authorize <span class="accent">combine.social</span> it will automatically delete the
+				token it has stored, and if you re-authorize multiple times it will just replace the old token,
+				so it&rsquo;s super easy to turn on and off if you ever change your mind.
+			</p>
+
+			<p>
+				The <a href="https://github.com/cyborch/toottail">source code</a> is also available if you want
+				to know what is going on or host the solution yourself.
+			</p>
+		</div>
+
+		<div>
+			<h2 id="signup">Sign Me Up!</h2>
+
+			<p><strong>Getting started is really easy:</strong></p>
+			<ol>
+				<li>Enter your mastodon instance name.</li>
+				<li>Authorise this application.</li>
+				<li>There is no step 3!</li>
+			</ol>
+			<Login />
+		</div>
+
+		<div class="footer">
+			<p><a href="/license">License</a></p>
+			<p><a href="/privacy">Privacy Policy</a></p>
+			<p>
+				&copy; {new Date().getFullYear()}{' '}
+				<a href="https://borch-andersen.com">Borch-Andersen ApS</a>
+			</p>
+		</div>
 	</div>
 </template>
 
---
+++ apps/web/src/routes/privacy/+page.svelte
@@ -0,0 +1,289 @@
+<template>
+	<div class="privacy">
+		<h1>Privacy policy</h1>
+
+		<h2>TL;DR</h2>
+
+		<p>
+			<strong
+				>The only thing we store is your username and an access token with read-only permissions.</strong
+			>
+		</p>
+
+		<p>
+			<strong
+				>We will never process any of your information other than what is minimally required to
+				ensure that remote message timelines are merged with your local timelines.</strong
+			>
+		</p>
+
+		<p>
+			<strong
+				>Your username and access token may be transfered to the server you specified when signing
+				up, but never to any other third parties.</strong
+			>
+		</p>
+
+		<p><strong>Nothing will ever be written to your account or posted on your behalf.</strong></p>
+
+		<h1>The Fine Print</h1>
+
+		<p>
+			We respect your privacy and are committed to protecting it through our compliance with this
+			privacy policy (“Policy”). This Policy describes the types of information we may collect from
+			you or that you may provide (“Personal Information”) on the <a href="https://combine.social"
+				>combine.social</a
+			> website (“Website” or “Service”) and any of its related products and services (collectively,
+			“Services”), and our practices for collecting, using, maintaining, protecting, and disclosing that
+			Personal Information. It also describes the choices available to you regarding our use of your
+			Personal Information and how you can access and update it.
+		</p>
+
+		<p>
+			This Policy is a legally binding agreement between you (“User”, “you” or “your”) and
+			Borch-Andersen ApS (“Borch-Andersen ApS”, “we”, “us” or “our”). If you are entering into this
+			agreement on behalf of a business or other legal entity, you represent that you have the
+			authority to bind such entity to this agreement, in which case the terms “User”, “you” or
+			“your” shall refer to such entity. If you do not have such authority, or if you do not agree
+			with the terms of this agreement, you must not accept this agreement and may not access and
+			use the Website and Services. By accessing and using the Website and Services, you acknowledge
+			that you have read, understood, and agree to be bound by the terms of this Policy. This Policy
+			does not apply to the practices of companies that we do not own or control, or to individuals
+			that we do not employ or manage.
+		</p>
+
+		<h2>Table of contents</h2>
+
+		<ol>
+			<li>Collection of personal information</li>
+			<li>Privacy of children</li>
+			<li>Use and processing of collected information</li>
+			<li>Disclosure of information</li>
+			<li>Retention of information</li>
+			<li>Links to other resources</li>
+			<li>Information security</li>
+			<li>Data breach</li>
+			<li>Changes and amendments</li>
+			<li>Acceptance of this policy</li>
+			<li>Contacting us</li>
+		</ol>
+
+		<h2>Collection of personal information</h2>
+
+		<p>
+			You can access and use the Website and Services without telling us who you are or revealing
+			any information by which someone could identify you as a specific, identifiable individual.
+			If, however, you wish to use some of the features offered on the Website, you may be asked to
+			provide certain Personal Information (for example, your username).
+		</p>
+
+		<p>
+			We receive and store any information you knowingly provide to us when you create an account,
+			or submit any forms on the Website. When required, this information may include account
+			details (such as username, unique user ID, name of server etc).
+		</p>
+
+		<p>
+			You can choose not to provide us with your Personal Information, but then you may not be able
+			to take advantage of some of the features on the Website. Users who are uncertain about what
+			information is mandatory are welcome to contact us.
+		</p>
+
+		<h2>Privacy of children</h2>
+
+		<p>
+			We do not knowingly collect any Personal Information from children under the age of 13. If you
+			are under the age of 13, please do not submit any Personal Information through the Website and
+			Services. If you have reason to believe that a child under the age of 13 has provided Personal
+			Information to us through the Website and Services, please contact us to request that we
+			delete that child's Personal Information from our Services.
+		</p>
+
+		<p>
+			We encourage parents and legal guardians to monitor their children's Internet usage and to
+			help enforce this Policy by instructing their children never to provide Personal Information
+			through the Website and Services without their permission. We also ask that all parents and
+			legal guardians overseeing the care of children take the necessary precautions to ensure that
+			their children are instructed to never give out Personal Information when online without their
+			permission.
+		</p>
+
+		<h2>Use and processing of collected information</h2>
+
+		<p>
+			We act as a data controller and a data processor when handling Personal Information, unless we
+			have entered into a data processing agreement with you in which case you would be the data
+			controller and we would be the data processor.
+		</p>
+
+		<p>
+			Our role may also differ depending on the specific situation involving Personal Information.
+			We act in the capacity of a data controller when we ask you to submit your Personal
+			Information that is necessary to ensure your access and use of the Website and Services. In
+			such instances, we are a data controller because we determine the purposes and means of the
+			processing of Personal Information.
+		</p>
+
+		<p>
+			We act in the capacity of a data processor in situations when you submit Personal Information
+			through the Website and Services. We do not own, control, or make decisions about the
+			submitted Personal Information, and such Personal Information is processed only in accordance
+			with your instructions. In such instances, the User providing Personal Information acts as a
+			data controller.
+		</p>
+
+		<p>
+			In order to make the Website and Services available to you, or to meet a legal obligation, we
+			may need to collect and use certain Personal Information. If you do not provide the
+			information that we request, we may not be able to provide you with the requested products or
+			services. Any of the information we collect from you may be used for the following purposes:
+		</p>
+
+		<ul>
+			<li>Create and manage user accounts</li>
+			<li>Run and operate the Website and Services</li>
+		</ul>
+
+		<p>
+			Processing your Personal Information depends on how you interact with the Website and
+			Services, where you are located in the world and if one of the following applies: (i) you have
+			given your consent for one or more specific purposes; (ii) provision of information is
+			necessary for the performance of an agreement with you and/or for any pre-contractual
+			obligations thereof; (iii) processing is necessary for compliance with a legal obligation to
+			which you are subject; (iv) processing is related to a task that is carried out in the public
+			interest or in the exercise of obcial authority vested in us; (v) processing is necessary for
+			the purposes of the legitimate interests pursued by us.
+		</p>
+
+		<p>
+			Note that under some legislations we may be allowed to process information until you object to
+			such processing by opting out, without having to rely on consent or any other of the legal
+			bases. In any case, we will be happy to clarify the specific legal basis that applies to the
+			processing, and in particular whether the provision of Personal Information is a statutory or
+			contractual requirement, or a requirement necessary to enter into a contract.
+		</p>
+
+		<h2>Managing information</h2>
+
+		<p>
+			You are able to delete certain Personal Information we have about you. The Personal
+			Information you can delete may change as the Website and Services change. When you delete
+			Personal Information, we will in short notice delete all Personal Information from our
+			records. If you would like to delete your Personal Information or permanently delete your
+			account, you can do so simply by deauthorising the application in your list of authorised
+			applications in your Mastodon Account.
+		</p>
+
+		<h2>Disclosure of information</h2>
+
+		<p>
+			We will never share any of your information with any of our our abliates, contracted
+			companies, and service providers (collectively, “Service Providers”). We will not share any
+			personally identifiable information with third parties and will not share any information with
+			unabliated third parties.
+		</p>
+
+		<h2>Retention of information</h2>
+
+		<p>
+			We will retain and use your Personal Information for the period necessary as long as your user
+			account remains active, to enforce our agreements, resolve disputes, and unless a longer
+			retention period is required or permitted by law.
+		</p>
+
+		<h2>Links to other resources</h2>
+
+		<p>
+			The Website and Services contain links to other resources that are not owned or controlled by
+			us. Please be aware that we are not responsible for the privacy practices of such other
+			resources or third parties. We encourage you to be aware when you leave the Website and
+			Services and to read the privacy statements of each and every resource that may collect
+			Personal Information.
+		</p>
+
+		<h2>Information security</h2>
+
+		<p>
+			We secure information you provide on computer servers in a controlled, secure environment,
+			protected from unauthorized access, use, or disclosure. We maintain reasonable administrative,
+			technical, and physical safeguards in an effort to protect against unauthorized access, use,
+			modification, and disclosure of Personal Information in our control and custody. However, no
+			data transmission over the Internet or wireless network can be guaranteed.
+		</p>
+
+		<p>
+			Therefore, while we strive to protect your Personal Information, you acknowledge that (i)
+			there are security and privacy limitations of the Internet which are beyond our control; (ii)
+			the security, integrity, and privacy of any and all information and data exchanged between you
+			and the Website and Services cannot be guaranteed; and (iii) any such information and data may
+			be viewed or tampered with in transit by a third party, despite best efforts.
+		</p>
+
+		<p>
+			As the security of Personal Information depends in part on the security of the device you use
+			to communicate with us and the security you use to protect your credentials, please take
+			appropriate measures to protect this information.
+		</p>
+
+		<h2>Data breach</h2>
+
+		<p>
+			In the event we become aware that the security of the Website and Services has been
+			compromised or Users' Personal Information has been disclosed to unrelated third parties as a
+			result of external activity, including, but not limited to, security attacks or fraud, we
+			reserve the right to take reasonably appropriate measures, including, but not limited to,
+			investigation and reporting, as well as notification to and cooperation with law enforcement
+			authorities. In the event of a data breach, we will make reasonable efforts to notify affected
+			individuals if we believe that there is a reasonable risk of harm to the User as a result of
+			the breach or if notice is otherwise required by law. When we do, we will post a notice on the
+			Website.
+		</p>
+
+		<h2>Changes and amendments</h2>
+
+		<p>
+			We reserve the right to modify this Policy or its terms related to the Website and Services at
+			any time at our discretion. When we do, we will revise the updated date at the bottom of this
+			page, post a notification on the main page of the Website. We may also provide notice to you
+			in other ways at our discretion, such as through the contact information you have provided.
+		</p>
+
+		<p>
+			An updated version of this Policy will be effective immediately upon the posting of the
+			revised Policy unless otherwise specified. Your continued use of the Website and Services
+			after the effective date of the revised Policy (or such other act specified at that time) will
+			constitute your consent to those changes. However, we will not, without your consent, use your
+			Personal Information in a manner materially different than what was stated at the time your
+			Personal Information was collected.
+		</p>
+
+		<h2>Acceptance of this policy</h2>
+
+		<p>
+			You acknowledge that you have read this Policy and agree to all its terms and conditions. By
+			accessing and using the Website and Services and submitting your information you agree to be
+			bound by this Policy. If you do not agree to abide by the terms of this Policy, you are not
+			authorized to access or use the Website and Services.
+		</p>
+
+		<h2>Contacting us</h2>
+
+		<p>
+			If you have any questions, concerns, or complaints regarding this Policy, the information we
+			hold about you, or if you wish to exercise your rights, we encourage you to contact us using
+			the details below:
+		</p>
+
+		<p><a href="mailto:info@borch-andersen.com">info@borch-andersen.com</a></p>
+
+		<p>
+			We will attempt to resolve complaints and disputes and make every reasonable effort to honor
+			your wish to exercise your rights as quickly as possible and in any event, within the
+			timescales provided by applicable data protection laws.
+		</p>
+
+		<p>This document was last updated on March 29, 2023</p>
+	</div>
+</template>
+
+<style src="./privacy.css"></style>
